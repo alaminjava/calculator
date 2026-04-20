2aca991a75f25272d1b1c3e267e3b3fdebd2428d
Al Amin Hossain <alaminhabibul@gmail.com>
2026-04-20 17:51:40 -0400

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const userInput = document.getElementById("calcDisplayID");
 const boxes = document.querySelectorAll(".box");
 
@@ -31,3 +32,38 @@ boxes.forEach(box => {
         }
     });
 });
+=======
+const userInput = document.getElementById("calcDisplayID");
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach(box => {
+    box.addEventListener("click", function () {
+        const value = this.dataset.value;
+
+        if (value === "C") {
+            userInput.value = "";
+        }
+
+        else if (value === "back") {
+            userInput.value = userInput.value.slice(0, -1);
+        }
+
+        else if (value === "=") {
+            try {
+                let result = userInput.value
+                    .replace(/÷/g, "/")
+                    .replace(/×/g, "*")
+                    .replace(/−/g, "-");
+
+                userInput.value = eval(result);
+            } catch {
+                userInput.value = "Error";
+            }
+        }
+
+        else {
+            userInput.value += value;
+        }
+    });
+});
+>>>>>>> 74adefa (update)
